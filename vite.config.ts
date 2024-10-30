982002130017f47e58b19171633937342dae6616
Mid1i <michac.tchernov@yandex.ru>
2024-10-30 19:37:13 +0300

    fix: Изменения в алиасах

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,23 @@ import vue from "@vitejs/plugin-vue";
 
 export default defineConfig({
   plugins: [vue()],
+  resolve: {
+    alias: {
+      "@/": "/src/",
+      "@/assets": "/src/assets",
+      "@/components": "/src/components",
+      "@/composables": "/src/composables",
+      "@/helpers": "/src/helpers",
+      "@/interfaces": "/src/interfaces",
+      "@/layouts": "/src/layouts",
+      "@/plugins": "/src/plugins",
+      "@/router": "/src/router",
+      "@/types": "/src/types",
+      "@/views": "/src/views"
+    }
+  },
+  server: {
+    host: true,
+    port: 5173
+  }
 })
